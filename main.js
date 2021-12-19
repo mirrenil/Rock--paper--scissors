38bf7333cb3884675fbac027183ed29f4f341051
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2021-12-19 20:23:21 +0100

    Still not working

--- main.js
+++ main.js
@@ -1,81 +1,106 @@
+const game = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+  let moves = 0;
+};
 
-const options = document.querySelectorAll(".options");
-let pScore = 0;
-let cScore = 0;
+const playGame = () => {
+  const rockBtn = document.querySelector(".rock");
+  const paperBtn = document.querySelector("paper");
+  const scissorBtn = document.querySelector("scissor");
+  const playerOptions = [rockBtn, paperBtn, scissorBtn];
+  const computerOptions = ["rock", "paper", "scissors"];
 
-options.forEach((option) => {
-  option.addEventListener("click", function () {
-    const pInput = this.value;
+  playerOptions.forEach((option) => {
+    option.addEventListener('click', function () {
+      const movesLeft = document.querySelector(".movesLeft");
+      moves++;
+      movesLeft.innerText = `Moves Left: ${10 - moves}`;
 
-    const cOptions = ["Rock", "Paper", "Scissors"];
-    const cInput = cOptions[Math.floor(Math.random() * 3)];
+      const choiceNumber = Math.floor(Math.random() * 3);
+      const computerChoice = computerOptions[choiceNumber];
 
-    updateMoves(pInput, cInput);
-    compareInputs(pInput, cInput);
-    updateScore();
-    if (checkWinner()) {
-      pScore = cScore = 0;
-      updateScore();
-    }
+      winner(this.innerText, computerChoice);
+
+      if (moves == 10) {
+        gamesOver(playerOptions, movesLeft);
+      }
+    });
   });
-});
+};
 
-function updateMoves(pInput, cInput) {
-    document.getElementById('p-move').src = `./assets/${pInput}.svg`;
-    document.getElementById('c-move').src = `./assets/${cInput}.svg`;
-}
-
-function compareInputs(pInput, cInput) {
-  const currentMatch = `${pInput} vs ${cInput}`;
-  if (pInput === cInput) {
-    alert(`${currentMatch} is a Tie`);
-    return;
-  }
-
-  if (pInput === "Rock") {
-    if (cInput === "Scissors") {
-      alert(`${currentMatch} = You Win`);
-      pScore++;
+const winner = (player,computer) => {
+  const result = document.querySelector('.result');
+  const playerScoreBoard = document.querySelector('.playerCount');
+  const computerScoreBoard = document.querySelector('.computerCount');
+  player = player.toLowerCase();
+  computer = computer.toLowerCase();
+  if (player === computer) {
+    result.textContent = "It's a tie!"
+  } else if (player == 'rock') {
+    if (computer == 'paper') {
+      result.textContent = "The computer won!";
+      computerScore++;
+      computerScoreBoard.textContent = computerScore;
     } else {
-      alert(`${currentMatch} = Computer Wins`);
-      cScore++;
+      result.textContent = "You won!";
+      playerScore++;
+      playerScoreBoard.textContent = playerScore;
+    }
+  } else if(player == 'scissors') {
+    if(computer == 'rock') {
+      result.textContent = 'The computer won!';
+      computerScore++;
+      computerScoreBoard.textContent = computerScore;
+    } else {
+      result.textContent = "You won!";
+      playerScore++;
+      playerScoreBoard.textContent = playerScore;
+    }
+  } else if(player == 'paper') {
+    if(computer == 'scissors') {
+      result.textContent = "The Computer won!";
+      computerScore++;
+      computerScoreBoard.textContent = computerScore;
+    } else {
+      result.textContent = 'You won!';
+      playerScore++;
+      playerScoreBoard.textContent = playerScore;
     }
   }
-  //Check for Paper
-  else if (pInput === "Paper") {
-    if (cInput === "Rock") {
-      alert(`${currentMatch} = You Win`);
-      pScore++;
-    } else {
-      alert(`${currentMatch} = Computer Wins`);
-      cScore++;
-    }
-  }
-  //Check for Scissors
-  else {
-    if (cInput === "Paper") {
-      alert(`${currentMatch} = You Win`);
-      pScore++;
-    } else {
-      alert(`${currentMatch} = Computer Wins`);
-      cScore++;
-    }
-  }
 }
 
-function updateScore() {
-  document.getElementById("p-score").textContent = pScore;
-  document.getElementById("c-score").textContent = cScore;
+const gameOver = (playerOptions,movesLeft) => {
+  const chooseMove = document.querySelector('.move');
+  const result = document.querySelector('.result');
+  const reloadBtn = document.querySelector('.reload');
+
+  playerOptions.forEach(option => {
+    option.style.display = 'none';
+  })
+  chooseMove.innerText = 'Game Over!';
+  movesLeft.style.display = 'none';
+
+  if (playerScore > computerScore) {
+    result.style.fontSize = '2rem';
+    result.style.color = '#308D46';
+    result.innerText = 'Congratulations, you have defeated the Computer!';
+  } else if (playerScore < computerScore) {
+    result.style.fontSize = '2rem';
+    result.style.color = 'red';
+    result.innerText = 'You lost the game!';
+  } else {
+    result.style.fontSize = '2rem';
+    result.style.color = 'grey';
+    result.innerText = "It's a tie!";
+  }
+  reloadBtn.innerText = 'Play again';
+  reloadBtn.style.display = 'flex';
+  reloadBtn.addEventListener('click', () => {
+    window.location.reload();
+  })
 }
 
-function checkWinner() {
-  if (pScore === 5 || cScore === 5) {
-    const winner =
-      pScore === 5
-        ? "You win the game! Congratulations!"
-        : "Computer wins the game! Try again next time!";
-    alert(winner);
-    return true;
-  }
-  return false;
-}
+playGame();
+
+game();
